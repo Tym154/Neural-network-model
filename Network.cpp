5cb5b1480f196def9bd4f3807ba6ade7a2e33356
Tym154 <kolumbuse@seznam.cz>
2024-06-07 22:09:40 +0200

    adding the Network constructor and adding some more comments

--- Network.cpp
+++ Network.cpp
@@ -25,10 +25,13 @@ class Node{
         mt19937 gen(rd()); //seed the generator
         uniform_real_distribution<> distr(-1.0, 1.0); //random double between -1 and 1
 
-        for(int i = 0; i < input_size; i++){
+        for(int i = 0; i < input_size; i++){ //setting random number between 1 and -1 for weights and a bias
             weights.push_back(distr(gen));
         }
         bias = (distr(gen));
+
+        value = 0.0; //reseting the values to 0 so they dont acumulate when learning
+        output = 0.0;
     }
 
     //using leaky relu for the activation function in hidden layers
@@ -71,6 +74,15 @@ class Layer{
 class Network{
     public:
     vector<Layer> layers;
+
+    //Network constructor
+    Network(vector<int> num_nodes){
+        layers.emplace_back(Layer(num_nodes[0], num_nodes[0])); //adding the input layer
+
+        for(int i = 1; i < num_nodes.size(); i++){
+            layers.emplace_back(Layer(num_nodes[i], num_nodes[i-1])); //adding the hidden layers and output layer
+        }
+    }
 };
 
 
